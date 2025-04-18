e6420bc077608be2f33aaad9cf36c23770d923a5
Diver0218 <jamin.bareis@gmail.com>
2025-04-18 18:04:39 +0200

    using BookDB in main

--- bibliotheksverwaltung/src/main/java/main/Bibliotheksverwaltung.java
+++ bibliotheksverwaltung/src/main/java/main/Bibliotheksverwaltung.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 import application.*;
 import domain.*;
 import plugins.*;
@@ -7,10 +9,15 @@ import plugins.*;
 public class Bibliotheksverwaltung {
 
     public static void main(String[] args) {
+    	
+    	System.out.println("Please enter DB Path:");
+    	Scanner sc = new Scanner(System.in);
+    	String dbPath = sc.next();
+    	sc.close();
             
         FrontendHandler frontend = new ConsoleFrontend();
         Menu menu = new Menu();
-        menu.registerAction(new ListBooksAction(new FakeBookDB(), frontend));
+        menu.registerAction(new ListBooksAction(new BookDB(dbPath), frontend));
         
         
         frontend.showMenu(menu);
