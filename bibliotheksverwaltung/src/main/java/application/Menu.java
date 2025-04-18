0c2af49b7879cbf48d7c07407f6abe9eb0130a3e
Diver0218 <jamin.bareis@gmail.com>
2025-04-18 14:47:06 +0200

    Structer for Menu Observer

---
+++ bibliotheksverwaltung/src/main/java/application/Menu.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+	
+	private List<MenuAction> actions;
+	
+	public Menu() {
+		actions = new ArrayList<MenuAction>();
+	}
+	
+	public void registerAction(MenuAction action) {
+		actions.add(action);
+	}
+	
+	public void unregisterAction(MenuAction action) {
+		actions.remove(action);
+	}
+	
+	public String getAllDescriptions() {
+		String returnString = "";
+		int i = 0;
+		for (MenuAction action : actions) {
+			returnString += String.valueOf(i) + "\t\t" + action.getDescription() + "\n";
+		}
+		return returnString;
+	}
+	
+}
---
+++ bibliotheksverwaltung/src/main/java/application/MenuAction.java
@@ -0,0 +1,7 @@
+package application;
+
+public interface MenuAction {
+	
+	public String getDescription();
+	public void executeAction();
+}
