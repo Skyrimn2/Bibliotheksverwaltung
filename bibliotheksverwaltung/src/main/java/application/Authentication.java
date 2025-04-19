78fc3e1fd141160d47f9ada25410bc7cd5a04dcb
Diver0218 <jamin.bareis@gmail.com>
2025-04-19 14:57:01 +0200

    interfaces for registration and authentication with hash methods for apssword

--- bibliotheksverwaltung/src/main/java/application/Authentication.java
+++ bibliotheksverwaltung/src/main/java/application/Authentication.java
@@ -1,5 +1,33 @@
 package application;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
 public interface Authentication {
     boolean authenticate(String username, String password);
+    public default byte[] hashPassword(String password){
+    	byte[] hash = null;
+    	
+    	SecureRandom random = new SecureRandom();
+    	byte[] salt = new byte[16];
+    	random.nextBytes(salt);
+    	PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+    	SecretKeyFactory factory = null;
+    	try {
+			factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+			hash = factory.generateSecret(spec).getEncoded();
+		} catch (InvalidKeySpecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return hash;
+	}
 }
