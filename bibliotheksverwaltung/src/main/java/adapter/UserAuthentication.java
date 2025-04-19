de2a828ec4ee0c8b050eff7748870599700095e1
Diver0218 <jamin.bareis@gmail.com>
2025-04-19 14:59:28 +0200

    concrete Classes for User Authentication and Registration

--- bibliotheksverwaltung/src/main/java/adapter/UserAuthentication.java
+++ bibliotheksverwaltung/src/main/java/adapter/UserAuthentication.java
@@ -1,13 +1,44 @@
 package adapter;
 
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+
 import application.Authentication;
+import application.DBHandler;
+import domain.User;
+
 
 public class UserAuthentication implements Authentication {
+	
+	DBHandler<User> db;
+	
+	public UserAuthentication(DBHandler<User> db) {
+		super();
+		this.db = db;
+	}
 
 	@Override
 	public boolean authenticate(String username, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		User user_db = db.getItemByString("name", username);
+		if(user_db == null) {
+			return false;
+		}
+		byte[] password_hash = null;
+		byte[] db_password_hash = null;
+		
+		password_hash = this.hashPassword(password);
+		db_password_hash = user_db.getPassword();
+
+		
+		if (Arrays.equals(password_hash, db_password_hash) && db_password_hash != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
 	}
 
 }
--- bibliotheksverwaltung/src/main/java/adapter/UserRegistration.java
+++ bibliotheksverwaltung/src/main/java/adapter/UserRegistration.java
@@ -1,13 +1,29 @@
 package adapter;
 
+
+
+import application.DBHandler;
 import application.Registration;
+import domain.User;
 
 public class UserRegistration implements Registration {
 
+	private DBHandler<User> db;
+	
+	public UserRegistration(DBHandler<User> db){
+		super();
+		this.db = db;
+	}
+	
 	@Override
 	public boolean register(String username, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		byte[] password_hash = this.hashPassword(password);
+		User user = new User(username, password_hash, 0, null);
+		
+		db.saveItem(user);
+		
+		return true;
+		
 	}
 
 }
