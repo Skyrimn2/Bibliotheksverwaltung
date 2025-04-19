d046510f430ffdcc581dfe6a1ce5fa11b9094e94
Diver0218 <jamin.bareis@gmail.com>
2025-04-19 14:57:52 +0200

    adding get item by String

--- bibliotheksverwaltung/src/main/java/application/DBHandler.java
+++ bibliotheksverwaltung/src/main/java/application/DBHandler.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public interface DBHandler<T> {
 
+	
 	public T loadItemByID(int id);
 	public void saveItem(T item);
 	public void updateItemByID(T item, int id);
 	public List<T> loadAllOfItem();
+	public T getItemByString(String column, String value);
 }
