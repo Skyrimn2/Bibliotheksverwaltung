338ef8c1b6808262d60f94f63bf91ec9148188d6
Diver0218 <jamin.bareis@gmail.com>
2025-04-19 14:59:52 +0200

    using login in main

--- bibliotheksverwaltung/src/main/java/main/Bibliotheksverwaltung.java
+++ bibliotheksverwaltung/src/main/java/main/Bibliotheksverwaltung.java
@@ -1,6 +1,14 @@
 package main;
 
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Scanner;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.*;
 
 import application.*;
 import domain.*;
@@ -9,15 +17,18 @@ import plugins.*;
 public class Bibliotheksverwaltung {
 
     public static void main(String[] args) {
+ 
+
     	
     	System.out.println("Please enter DB Path:");
     	Scanner sc = new Scanner(System.in);
     	String dbPath = sc.next();
             
-        FrontendHandler frontend = new ConsoleFrontend();
+        FrontendHandler frontend = new ConsoleFrontend(dbPath);
         Menu menu = new Menu();
         menu.registerAction(new ListBooksAction(new BookDB(dbPath), frontend));
         
+        frontend.loginView();        
         
         frontend.showMenu(menu);
         int option = frontend.readMenuOption();
