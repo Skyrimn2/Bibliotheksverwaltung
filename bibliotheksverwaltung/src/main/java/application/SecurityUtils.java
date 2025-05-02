f7567058099cf8040b3a12dfa2ba4b8e0f42d455
Skyrim <131873263+Skyrimn2@users.noreply.github.com>
2025-05-02 09:35:12 +0200

    Adding SecurityUtils class for removing redundancy

--- bibliotheksverwaltung/src/main/java/application/SecurityUtils.java
+++ bibliotheksverwaltung/src/main/java/application/SecurityUtils.java
@@ -1,5 +1,41 @@
 package application;
 
-public class SecurityUtils {
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class SecurityUtils {
+    
+    private static final int ITERATIONS = 65536;
+    private static final int KEY_LENGTH = 128;
+    private static final int SALT_SIZE = 16;
+    
+    private SecurityUtils() {
+        // Private Konstruktor, um Instanziierung zu verhindern
+    }
+    
+    public static byte[] hashPassword(String password, byte[] salt) {
+        byte[] hash = null;
+        
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, ITERATIONS, KEY_LENGTH);
+        SecretKeyFactory factory = null;
+        try {
+            factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            hash = factory.generateSecret(spec).getEncoded();
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        return hash;
+    }
+    
+    public static byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_SIZE];
+        random.nextBytes(salt);
+        return salt;
+    }
 }
