15b5bcf6a5f3aa8706da6e0537c47f9e67441e75
Skyrim <131873263+Skyrimn2@users.noreply.github.com>
2025-05-02 10:38:28 +0200

    Removing Redundancy

--- bibliotheksverwaltung/src/main/java/application/Registration.java
+++ bibliotheksverwaltung/src/main/java/application/Registration.java
@@ -1,37 +1,16 @@
 package application;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
-
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-
 public interface Registration {
 
-    public boolean register(String username, String password);
-    public default byte[] hashPassword(String password, byte[] salt) {
-    	byte[] hash = null;
+    boolean register(String username, String password);
 
-    	PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-    	SecretKeyFactory factory = null;
-		try {
-			factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-			hash = factory.generateSecret(spec).getEncoded();
-		} catch (InvalidKeySpecException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return hash;
-	}
-    public default byte[] generateSalt() {
-    	SecureRandom random = new SecureRandom();
-    	byte[] salt = new byte[16];
-    	random.nextBytes(salt);
-    	return salt;
+    @Deprecated
+    default byte[] hashPassword(String password, byte[] salt) {
+        return SecurityUtils.hashPassword(password, salt);
+    }
+    
+    @Deprecated
+    default byte[] generateSalt() {
+        return SecurityUtils.generateSalt();
     }
 }
