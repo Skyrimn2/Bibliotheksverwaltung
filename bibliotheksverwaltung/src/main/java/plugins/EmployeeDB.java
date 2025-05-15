d5de02aecb241366ab957fd3a378347a24e5f00b
Sebastian <sebbel2002@gmail.com>
2025-05-15 11:10:21 +0200

    Fixing Error

--- bibliotheksverwaltung/src/main/java/plugins/EmployeeDB.java
+++ bibliotheksverwaltung/src/main/java/plugins/EmployeeDB.java
@@ -4,89 +4,164 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import adapter.DBHandlerConnection;
+import application.DatabaseException;
 import domain.Employee;
 
 public class EmployeeDB extends DBHandlerConnection<Employee> {
 
-	public EmployeeDB(String indbPath) {
-		super(indbPath);
-	}
+    public EmployeeDB(String indbPath) {
+        super(indbPath);
+    }
 
-	@Override
-	public Employee loadItemByID(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Employee loadItemByID(int id) {
+        try {
+            String sql = "SELECT * FROM EMPLOYEES WHERE id = ?";
+            Connection conn = this.conn();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            ResultSet result = pstmt.executeQuery();
 
-	@Override
-	public void saveItem(Employee item) {
-		try {
+            if (!result.next()) {
+                conn.close();
+                return null;
+            }
 
-			String sql = "INSERT INTO EMPLOYEES(name, password, salt) VALUES (?, ?, ?)";
+            Employee emp = new Employee(
+                result.getString("name"),
+                result.getBytes("password"),
+                result.getInt("id"),
+                result.getBytes("salt")
+            );
+            conn.close();
+            return emp;
+        } catch (SQLException | DatabaseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-			Connection conn = this.conn();
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, item.getName());
-			pstmt.setBytes(2, item.getPassword());
-			pstmt.setBytes(3, item.getSalt());
-			pstmt.executeUpdate();
+    @Override
+    public void saveItem(Employee item) {
+        try {
+            String sql = "INSERT INTO EMPLOYEES(name, password, salt) VALUES (?, ?, ?)";
+            Connection conn = this.conn();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, item.getName());
+            pstmt.setBytes(2, item.getPassword());
+            pstmt.setBytes(3, item.getSalt());
+            pstmt.executeUpdate();
+            conn.close();
+        } catch (SQLException | DatabaseException e) {
+            e.printStackTrace();
+        }
+    }
 
-			conn.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void updateItemByID(Employee item, int id) {
+        try {
+            String sql = "UPDATE EMPLOYEES SET name = ?, password = ?, salt = ? WHERE id = ?";
+            Connection conn = this.conn();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, item.getName());
+            pstmt.setBytes(2, item.getPassword());
+            pstmt.setBytes(3, item.getSalt());
+            pstmt.setInt(4, id);
+            pstmt.executeUpdate();
+            conn.close();
+        } catch (SQLException | DatabaseException e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void updateItemByID(Employee item, int id) {
-		// TODO Auto-generated method stub
+    @Override
+    public List<Employee> loadAllOfItem() {
+        List<Employee> employees = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM EMPLOYEES";
+            Connection conn = this.conn();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet result = pstmt.executeQuery();
 
-	}
+            while (result.next()) {
+                Employee emp = new Employee(
+                    result.getString("name"),
+                    result.getBytes("password"),
+                    result.getInt("id"),
+                    result.getBytes("salt")
+                );
+                employees.add(emp);
+            }
+            conn.close();
+        } catch (SQLException | DatabaseException e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
 
-	@Override
-	public List<Employee> loadAllOfItem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Employee getItemByString(String column, String value) {
+        try {
+            if (!column.equals("name") && !column.equals("id")) {
+                throw new IllegalArgumentException("Ungültiger Spaltenname");
+            }
 
-	@Override
-	public Employee getItemByString(String column, String value) {
-		try {
+            String sql = "SELECT * FROM EMPLOYEES WHERE " + column + " = ?";
+            Connection conn = this.conn();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, value);
+            ResultSet result = pstmt.executeQuery();
 
-			if (!column.equals("name") && !column.equals("id")) {
-		        throw new IllegalArgumentException("Ungültiger Spaltenname");
-		    }
+            if (!result.next()) {
+                conn.close();
+                return null;
+            }
 
-		    String sql = "SELECT * FROM USERS WHERE " + column + " = ?";
+            Employee emp = new Employee(
+                result.getString("name"),
+                result.getBytes("password"),
+                result.getInt("id"),
+                result.getBytes("salt")
+            );
+            conn.close();
+            return emp;
+        } catch (SQLException | DatabaseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-			Connection conn = this.conn();
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, value);
-			ResultSet result = pstmt.executeQuery();
+    @Override
+    public List<Employee> getItemsByString(String column, String value) {
+        List<Employee> employees = new ArrayList<>();
+        try {
+            if (!column.equals("name") && !column.equals("id")) {
+                throw new IllegalArgumentException("Ungültiger Spaltenname");
+            }
 
-			if (!result.next()) {
-				return null;
-			}
+            String sql = "SELECT * FROM EMPLOYEES WHERE " + column + " LIKE ?";
+            Connection conn = this.conn();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, "%" + value + "%");
+            ResultSet result = pstmt.executeQuery();
 
-
-			Employee emp = new Employee(result.getString("name"), result.getBytes("password"), result.getInt("id"), result.getBytes("salt"));
-			conn.close();
-			return emp;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	@Override
-	public List<Employee> getItemsByString(String column, String value) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+            while (result.next()) {
+                Employee emp = new Employee(
+                    result.getString("name"),
+                    result.getBytes("password"),
+                    result.getInt("id"),
+                    result.getBytes("salt")
+                );
+                employees.add(emp);
+            }
+            conn.close();
+        } catch (SQLException | DatabaseException e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
 }
