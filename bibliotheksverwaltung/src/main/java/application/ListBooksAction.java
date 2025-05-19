08f9831a34af83edfe74642e74207584538333e1
Diver0218 <jamin.bareis@gmail.com>
2025-05-19 21:24:14 +0200

    example Usage of Factory

--- bibliotheksverwaltung/src/main/java/application/ListBooksAction.java
+++ bibliotheksverwaltung/src/main/java/application/ListBooksAction.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 
-import adapter.DisplayableBook;
 import domain.Book;
 import domain.Displayable;
 
@@ -12,6 +11,7 @@ public class ListBooksAction implements MenuAction{
 	private final String description = "List all Books";
 	private DBHandler<Book> bookDB;
 	private FrontendHandler frontend;
+	private IDisplayableFactory displayableFactory;
 
 	public ListBooksAction(DBHandler<Book> bookDB, FrontendHandler frontend) {
 		super();
@@ -24,7 +24,7 @@ public class ListBooksAction implements MenuAction{
 		List<Book> allBooks = bookDB.loadAllOfItem();
 		List<Displayable> disps = new ArrayList<>();
 		for (Book b: allBooks) {
-			disps.add(new DisplayableBook(b));
+			disps.add(displayableFactory.createDisplayableBook(b));
 		}
 		frontend.showResultList(disps);
 	}
